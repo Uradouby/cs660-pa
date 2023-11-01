a470d00e658d9984afcd6769084fc99ab7819f9b
Aneesh Raman <aneeshraman97@gmail.com>
2023-11-01 11:05:27 -0400

    added test files

--- tests/pa2/BTreeFile_test.cpp
+++ tests/pa2/BTreeFile_test.cpp
@@ -4,24 +4,48 @@
 #include <db/Utility.h>
 #include <db/IntField.h>
 
-TEST(BTreeFileTest, findLeafPage) {
+TEST(BTreeFileTest, splitLeafPagesTest) {
     db::Database::reset();
-    db::BufferPool &bufferpool = db::Database::getBufferPool();
     db::Catalog &catalog = db::Database::getCatalog();
-    db::TupleDesc td = db::Utility::getTupleDesc(340);
+    db::TupleDesc td = db::Utility::getTupleDesc(2);
     db::BTreeFile file("btree.dat", 0, td);
     catalog.addTable(&file);
     db::TransactionId tid;
-    db::Tuple tup(file.getTupleDesc());
 
-    for (int i = 0; i < td.numFields(); i++) {
-        tup.setField(i, new db::IntField(i));
+    for(int i = 0; i < 600; i++)
+    {
+        db::Tuple tup(td);
+        for (int j = 0; j < td.numFields(); j++) {
+            tup.setField(j, new db::IntField(i));
+        }
+        file.insertTuple(tid, tup);
     }
-    file.insertTuple(tid, tup);
-    auto items = file.iterable(tid);
-    int count = 0;
-    for (auto it : items) {
-        count++;
-        std::cout << count << ' ' << it.to_string() << std::endl;
+
+    ASSERT_EQ(file.getNumPages(), 3);
+}
+
+TEST(BTreeFileTest, findLeafPagesTest) {
+    db::Database::reset();
+    db::BufferPool &bufferpool = db::Database::getBufferPool();
+    db::Catalog &catalog = db::Database::getCatalog();
+    db::TupleDesc td = db::Utility::getTupleDesc(2);
+    db::BTreeFile file("btree1.dat", 0, td);
+    catalog.addTable(&file);
+    db::TransactionId tid;
+
+    for(int i = 0; i < 1000; i++)
+    {
+        db::Tuple tup(td);
+        for (int j = 0; j < td.numFields(); j++) {
+            tup.setField(j, new db::IntField(i));
+        }
+        file.insertTuple(tid, tup);
     }
+
+    db::PagesMap dirtypages{};
+    db::BTreeRootPtrPage *rootPtr = file.getRootPtrPage(tid, dirtypages);
+    db::BTreePageId *rootId = rootPtr->getRootId();
+    db::IntField f(5);
+    db::BTreeLeafPage *leaf_page = file.findLeafPage(tid, rootId, db::Permissions::READ_ONLY,&f);
+    ASSERT_EQ(leaf_page->getId().pageNumber(), 1);
 }
